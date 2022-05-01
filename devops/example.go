76bdf52bfd8cfc8e7576af12340c43ae0d870edd
Zygaa <patrykzygmunt10@gmail.com>
2022-05-01 19:56:32 +0200

    dziala

---
+++ devops/example.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main()  {
+  var challenge = "#90DaysOfDevOps"
+  const daysTotal = 90
+  var daysComplete = 11
+}
---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main()  {
+  fmt.Println("Hello GO")
+  var x int
+  fmt.Println(x)
+  a := [5]int{5, 4, 3, 2, 1}
+  fmt.Println(a)
+}
