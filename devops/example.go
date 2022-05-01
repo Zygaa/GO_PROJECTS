0f3f7b2036717290f847cb446b7eb1b0a79af9bd
Zygaa <patrykzygmunt10@gmail.com>
2022-05-01 21:44:00 +0200

    hi

--- devops/example.go
+++ devops/example.go
@@ -4,7 +4,10 @@ func main()  {
   var challenge string = "#90DaysOfDevOps"
   const daysTotal uint = 90
   var daysComplete uint = 11
-  fmt.Println("Welcome to", challenge)
+  var name string
+  fmt.Println("Enter your name:")
+  fmt.Scanln(&name)
+  fmt.Println("Welcome to", challenge, name)
   fmt.Println("This is a", daysTotal, "challenge and you have completed", daysComplete, "days")
   fmt.Println("Great work!")
 }
