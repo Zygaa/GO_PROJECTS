4c68f7d0fe847ee44ea29f99aa68995160bdd78c
Zygaa <patrykzygmunt10@gmail.com>
2022-05-01 21:34:56 +0200

    1

--- devops/example.go
+++ devops/example.go
@@ -1,7 +1,10 @@
 package main
 import "fmt"
 func main()  {
-  var challenge = "#90DaysOfDevOps"
-  const daysTotal = 90
-  var daysComplete = 13
+  var challenge string = "#90DaysOfDevOps"
+  const daysTotal uint = 90
+  var daysComplete uint = 11
+  fmt.Println("Welcome to", challenge)
+  fmt.Println("This is a", daysTotal, "challenge and you have completed", daysComplete, "days")
+  fmt.Println("Great work!")
 }
